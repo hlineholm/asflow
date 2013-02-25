f6a6528d49baae03570e8f1a66f2980a4b74ff50
hholmgren <hholmgren@me.com>
2013-02-25 13:35:36 +0100

    Updated to recent updates
    Updated to recent update of the same file on GitHub.

--- testing/js/asFlow.js
+++ testing/js/asFlow.js
@@ -1,39 +1,4 @@
-// **************************************************************************************** Example of function in the functions array
-//
-// var function0 = function(optionalArgs0, optionalArgs1 ... optionalArgsN, call) {
-//			call.currentResults(); -> array of collected results at the moment sortet as functions array
-//			call.parentResult(); -> result passed by function above current in functions array
-//			call.resultByFunction(i); -> result passed by specific function from the functions array
-//
-//			call.back(optionalResult); -> Is required when the function returns and it will deliver the result to the results array
-//		};
-//
-//
-// ***************************************************************************************************** Example of the function array
-//
-// var functions = [
-//			function0,
-//			function1, [argument0, argument1 ... argumentN],
-//			function3,
-//			function4,
-//			function5, [argument0, argument1 ... argumentN]
-//		];
-//
-//
-// ******************************************************************************************* Example of executing the flow functions
-//
-// var optionalCallback = function(results) {
-//		Do what anything with the results array if you choose!
-// };
-// asyncFlow.serialExecute(functions, optionalCallback);
-// asyncFlow.parallelExecute(functions, optionalCallback);
-// asyncFlow.parallelExecute_withLimit(functions, 3, optionalCallback);
-//
-//
-// ***********************************************************************************************************************************
-
-
-var asyncFlow = (function() {
+var asFlow = (function() {
 	"use strict";
 	var spliceAndVerify, serialExecute, parallelExecute, parallelExecute_withLimit;
 	spliceAndVerify = function(functions) {
